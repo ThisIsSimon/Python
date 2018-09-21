0ca31d78b8d8a8455afe8ae206ac472324684061
Simon Yee <37600643+ThisIsSimon@users.noreply.github.com>
2018-09-21 14:26:08 -0700

    Update two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -1,24 +1,19 @@
-# https://leetcode.com/problems/reverse-integer/description/
-
-# [key : value]
-# [input_num: reversed_num
-# Will need to improve later to:
-# 1. not convert to string
-
-
-x = str(x)
-dictionary = {x: ''}
-
-
-class Solution:
-    def is_palindrome(x):
-        for i in range(len(x) - 1, -1, -1):
-            dictionary[x] += x[i]
-
-        if dictionary[x] in dictionary and:
-            return True
-        else:
-            return False
-
-
-print(Solution.is_palindrome(x))
+'''
+https://leetcode.com/problems/two-sum/description/
+ Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ Example:
+ Given nums = [2, 7, 11, 15], target = 9,
+ Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+'''
+ def two_sum(list, target):
+    for x in range(len(list)):
+        for y in range(x + 1, len(list)):
+            if  list[x] + list[y] == target:
+                return[list.index(list[x]), list.index(list[y])]
+            else:
+                continue
+ nums = [2, 7, 11, 15]
+target = 9
+ print(two_sum(nums, target))
