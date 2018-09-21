1e0bc9a86a0267c2204336d228ad78f628007f57
Simon Yee <37600643+ThisIsSimon@users.noreply.github.com>
2018-09-21 14:27:16 -0700

    Update palindrome_number.py

--- palindrome_number.py
+++ palindrome_number.py
@@ -1,13 +1,9 @@
 # https://leetcode.com/problems/reverse-integer/description/
-# Given a 32-bit signed integer, reverse digits of an integer.
-
-# (2 ** 31) - 1 # Max 32 bit integer
 
 # [key : value]
 # [input_num: reversed_num
 # Will need to improve later to:
 # 1. not convert to string
-# 2. limit integer to 32 bit and return 0 if value exceeds range
 
 
 x = str(x)
