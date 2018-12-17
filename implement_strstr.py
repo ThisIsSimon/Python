d5f9bdf2282f017e756c23d2fc13cef3fb5d9c63
Simon Yee <37600643+ThisIsSimon@users.noreply.github.com>
2018-12-17 10:50:45 -0800

    Create implement_strstr.py

---
+++ implement_strstr.py
@@ -0,0 +1,20 @@
+haystack = "aaaaa"
+needle = "bba"
+
+
+class Solution:
+    def strStr(haystack, needle):
+
+        if len(needle) == 0:
+            return 0
+
+        string = ""
+        for char in range(len(haystack)):
+            string += haystack[char]
+            if needle in string:
+                return char - (len(needle) - 1)
+        else:
+            return -1
+
+
+print(Solution.strStr(haystack, needle))
